35dc6bebd8d521dc786c60b8f402caebc7674099
tomms25 <tommasobaccano@gmail.com>
2023-02-06 16:42:48 +0100

    correzione factory

--- database/factories/TaskFactory.php
+++ database/factories/TaskFactory.php
@@ -17,11 +17,10 @@ class TaskFactory extends Factory
     public function definition()
     {
         return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
-            'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-            'remember_token' => Str::random(10),
+            'firstName' => fake()->firstName(),
+            'lastName' => fake()->lastName(),
+            'dateOfBirth' => fake()->dateTime(),
+            'height' => fake()->numberBetween(1, 200),
         ];
     }
 }
